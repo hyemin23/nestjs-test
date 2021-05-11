669214535d586c6de0b99e81164ad6793bf564aa
hyemin23 <hm_stom@naver.com>
2021-05-12 01:28:17 +0900

    #유효성 검사 라이브러리 추가

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    //알수없는 속성값의 요청 자체를 막아버리는 역할
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      //실제 타입으로 바꿔줌
+      transform: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
